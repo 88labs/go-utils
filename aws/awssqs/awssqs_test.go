64d91dce323069a8fa68e980e9e4b8961c182a3b
tom twinkle <tomtinkle29@gmail.com>
2023-02-17 11:29:18 +0900

    feat: SendMessageGob(), ReceiveMessageGob()

--- aws/awssqs/awssqs_test.go
+++ aws/awssqs/awssqs_test.go
@@ -92,7 +92,52 @@ func TestSendMessage(t *testing.T) {
 	})
 }
 
-func TestRetrieveAndDeleteMessage(t *testing.T) {
+func TestSendMessageGob(t *testing.T) {
+	ctx := ctxawslocal.WithContext(
+		context.Background(),
+		ctxawslocal.WithAccessKey("DUMMYACCESSKEYEXAMPLE"),
+		ctxawslocal.WithSecretAccessKey("DUMMYSECRETKEYEXAMPLE"),
+		ctxawslocal.WithSQSEndpoint("http://127.0.0.1:4566"),
+	)
+
+	t.Cleanup(Cleanup)
+	type TestMessageBody struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	t.Run("SendMessage", func(t *testing.T) {
+		message := TestMessageBody{
+			ID:   1,
+			Name: faker.Name(),
+		}
+		res, err := awssqs.SendMessageGob(ctx, TestRegion, TestQueue, message)
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotEmpty(t, res.MessageId)
+	})
+	t.Run("SendMessage duplicate message 3", func(t *testing.T) {
+		message := TestMessageBody{
+			ID:   1,
+			Name: "duplicate message",
+		}
+		uniq := make(map[string]struct{})
+		for i := 0; i < 3; i++ {
+			res, err := awssqs.SendMessageGob(ctx, TestRegion, TestQueue, message)
+			if !assert.NoError(t, err) {
+				return
+			}
+			t.Log(res.MessageId)
+			if i > 0 {
+				_, ok := uniq[*res.MessageId]
+				assert.False(t, ok)
+			}
+			uniq[*res.MessageId] = struct{}{}
+		}
+	})
+}
+
+func TestReceiveAndDeleteMessage(t *testing.T) {
 	ctx := ctxawslocal.WithContext(
 		context.Background(),
 		ctxawslocal.WithAccessKey("DUMMYACCESSKEYEXAMPLE"),
@@ -153,6 +198,89 @@ func TestRetrieveAndDeleteMessage(t *testing.T) {
 		})
 		eg.Go(func() error {
 			res, err := awssqs.ReceiveMessage(ctx, TestRegion, TestQueue,
+				sqsreceive.WithWaitTimeSeconds(0),
+				sqsreceive.WithVisibilityTimeout(5),
+			)
+			if err != nil {
+				return err
+			}
+			for _, v := range res.Messages {
+				messageID2 = *v.MessageId
+			}
+			return nil
+		})
+		assert.NoError(t, eg.Wait())
+		assert.NotEqual(t, messageID1, messageID2)
+	})
+}
+
+func TestReceiveGobAndDeleteMessage(t *testing.T) {
+	ctx := ctxawslocal.WithContext(
+		context.Background(),
+		ctxawslocal.WithAccessKey("DUMMYACCESSKEYEXAMPLE"),
+		ctxawslocal.WithSecretAccessKey("DUMMYSECRETKEYEXAMPLE"),
+		ctxawslocal.WithSQSEndpoint("http://127.0.0.1:4566"),
+	)
+
+	t.Cleanup(Cleanup)
+	type TestMessageBody struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	var count int
+	createFixture := func(t *testing.T, ctx context.Context) {
+		count++
+		message := TestMessageBody{
+			ID:   count,
+			Name: faker.Name(),
+		}
+		_, err := awssqs.SendMessageGob(ctx, TestRegion, TestQueue, message)
+		assert.NoError(t, err)
+	}
+
+	t.Run("Retrieve And DeleteMessage:1 message", func(t *testing.T) {
+		createFixture(t, ctx)
+		items, res, err := awssqs.ReceiveMessageGob(ctx, TestRegion, TestQueue, TestMessageBody{}, sqsreceive.WithWaitTimeSeconds(0))
+		if !assert.NoError(t, err) {
+			return
+		}
+		if assert.Greater(t, len(items), 0) {
+			for _, v := range items {
+				assert.NotEmpty(t, v.ID)
+				assert.NotEmpty(t, v.Name)
+			}
+		}
+		if assert.Greater(t, len(res.Messages), 0) {
+			for _, m := range res.Messages {
+				err := awssqs.DeleteMessage(ctx, TestRegion, TestQueue, m)
+				assert.NoError(t, err)
+			}
+		}
+	})
+
+	t.Run("Do not get the same queue during VisibilityTimeout", func(t *testing.T) {
+		createFixture(t, ctx)
+		createFixture(t, ctx)
+		time.Sleep(1 * time.Second)
+		var (
+			eg                     errgroup.Group
+			messageID1, messageID2 string
+		)
+		eg.Go(func() error {
+			_, res, err := awssqs.ReceiveMessageGob(ctx, TestRegion, TestQueue, TestMessageBody{},
+				sqsreceive.WithWaitTimeSeconds(0),
+				sqsreceive.WithVisibilityTimeout(5),
+			)
+			if err != nil {
+				return err
+			}
+			for _, v := range res.Messages {
+				messageID1 = *v.MessageId
+			}
+			return nil
+		})
+		eg.Go(func() error {
+			_, res, err := awssqs.ReceiveMessageGob(ctx, TestRegion, TestQueue, TestMessageBody{},
 				sqsreceive.WithWaitTimeSeconds(0),
 				sqsreceive.WithVisibilityTimeout(5),
 			)
