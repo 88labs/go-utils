d82bc09397d75dd3adcc31ede11f3c2d1ca37e75
tom twinkle <tomtinkle29@gmail.com>
2023-06-20 16:17:08 +0900

    feat: awss3 add global http dialer options

--- aws/awss3/client.go
+++ aws/awss3/client.go
@@ -3,8 +3,12 @@ package awss3
 import (
 	"context"
 	"fmt"
+	"net"
+
+	"github.com/88labs/go-utils/aws/awss3/options/global/s3dialer"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -13,6 +17,9 @@ import (
 	"github.com/88labs/go-utils/aws/ctxawslocal"
 )
 
+// GlobalDialer Global http dialer settings for awss3 library
+var GlobalDialer *s3dialer.ConfGlobalDialer
+
 // GetClient
 // Get s3 client for aws-sdk-go v2.
 // Using ctxawslocal.WithContext, you can make requests for local mocks
@@ -20,8 +27,26 @@ func GetClient(ctx context.Context, region awsconfig.Region) (*s3.Client, error)
 	if localProfile, ok := getLocalEndpoint(ctx); ok {
 		return getClientLocal(ctx, *localProfile)
 	}
+	awsHttpClient := awshttp.NewBuildableClient()
+	if GlobalDialer != nil {
+		awsHttpClient.WithDialerOptions(func(dialer *net.Dialer) {
+			if GlobalDialer.Timeout != 0 {
+				dialer.Timeout = GlobalDialer.Timeout
+			}
+			if GlobalDialer.Deadline != nil {
+				dialer.Deadline = *GlobalDialer.Deadline
+			}
+			if GlobalDialer.KeepAlive != 0 {
+				dialer.KeepAlive = GlobalDialer.KeepAlive
+			}
+		})
+	}
 	// S3 Client
-	awsCfg, err := awsConfig.LoadDefaultConfig(ctx, awsConfig.WithRegion(region.String()))
+	awsCfg, err := awsConfig.LoadDefaultConfig(
+		ctx,
+		awsConfig.WithRegion(region.String()),
+		awsConfig.WithHTTPClient(awsHttpClient),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("unable to load SDK config, %w", err)
 	}
@@ -42,7 +67,22 @@ func getClientLocal(ctx context.Context, localProfile LocalProfile) (*s3.Client,
 		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
 		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
 	})
+	awsHttpClient := awshttp.NewBuildableClient()
+	if GlobalDialer != nil {
+		awsHttpClient.WithDialerOptions(func(dialer *net.Dialer) {
+			if GlobalDialer.Timeout != 0 {
+				dialer.Timeout = GlobalDialer.Timeout
+			}
+			if GlobalDialer.Deadline != nil {
+				dialer.Deadline = *GlobalDialer.Deadline
+			}
+			if GlobalDialer.KeepAlive != 0 {
+				dialer.KeepAlive = GlobalDialer.KeepAlive
+			}
+		})
+	}
 	awsCfg, err := awsConfig.LoadDefaultConfig(ctx,
+		awsConfig.WithHTTPClient(awsHttpClient),
 		awsConfig.WithEndpointResolverWithOptions(customResolver),
 		awsConfig.WithCredentialsProvider(credentials.StaticCredentialsProvider{
 			Value: aws.Credentials{
