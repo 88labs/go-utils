bb9d781823d7920292cc4f46e4f29567e203591c
tom twinkle <tomtinkle29@gmail.com>
2024-01-19 12:31:52 +0900

    add global s3dialer

--- aws/awss3/client.go
+++ aws/awss3/client.go
@@ -5,14 +5,26 @@ import (
 	"context"
 	"encoding/gob"
 	"fmt"
+	"net"
+	"sync"
 
-	"github.com/88labs/go-utils/aws/awsconfig"
-
-	"github.com/88labs/go-utils/aws/ctxawslocal"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+	"github.com/88labs/go-utils/aws/awsconfig"
+	"github.com/88labs/go-utils/aws/awss3/options/global/s3dialer"
+	"github.com/88labs/go-utils/aws/ctxawslocal"
+)
+
+var (
+	// GlobalDialer Global http dialer settings for awss3 library
+	GlobalDialer *s3dialer.ConfGlobalDialer
+
+	customMu             sync.Mutex
+	customEndpointClient *s3.Client
 )
 
 // GetClient
@@ -20,12 +32,34 @@ import (
 // Using ctxawslocal.WithContext, you can make requests for local mocks
 func GetClient(ctx context.Context, region awsconfig.Region) (*s3.Client, error) {
 	if localProfile, ok := getLocalEndpoint(ctx); ok {
-		return getClientLocal(ctx, *localProfile)
+		customMu.Lock()
+		defer customMu.Unlock()
+		var err error
+		if customEndpointClient != nil {
+			return customEndpointClient, err
+		}
+		customEndpointClient, err = getClientLocal(ctx, *localProfile)
+		return customEndpointClient, err
+	}
+	awsHttpClient := awshttp.NewBuildableClient()
+	if GlobalDialer != nil {
+		awsHttpClient.WithDialerOptions(func(dialer *net.Dialer) {
+			if GlobalDialer.Timeout != 0 {
+				dialer.Timeout = GlobalDialer.Timeout
+			}
+			if GlobalDialer.Deadline != nil {
+				dialer.Deadline = *GlobalDialer.Deadline
+			}
+			if GlobalDialer.KeepAlive != 0 {
+				dialer.KeepAlive = GlobalDialer.KeepAlive
+			}
+		})
 	}
 	// S3 Client
 	awsCfg, err := awsConfig.LoadDefaultConfig(
 		ctx,
 		awsConfig.WithRegion(region.String()),
+		awsConfig.WithHTTPClient(awsHttpClient),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("unable to load SDK config, %w", err)
@@ -34,7 +68,22 @@ func GetClient(ctx context.Context, region awsconfig.Region) (*s3.Client, error)
 }
 
 func getClientLocal(ctx context.Context, localProfile LocalProfile) (*s3.Client, error) {
+	awsHttpClient := awshttp.NewBuildableClient()
+	if GlobalDialer != nil {
+		awsHttpClient.WithDialerOptions(func(dialer *net.Dialer) {
+			if GlobalDialer.Timeout != 0 {
+				dialer.Timeout = GlobalDialer.Timeout
+			}
+			if GlobalDialer.Deadline != nil {
+				dialer.Deadline = *GlobalDialer.Deadline
+			}
+			if GlobalDialer.KeepAlive != 0 {
+				dialer.KeepAlive = GlobalDialer.KeepAlive
+			}
+		})
+	}
 	awsCfg, err := awsConfig.LoadDefaultConfig(ctx,
+		awsConfig.WithHTTPClient(awsHttpClient),
 		awsConfig.WithCredentialsProvider(credentials.StaticCredentialsProvider{
 			Value: aws.Credentials{
 				AccessKeyID:     localProfile.AccessKey,
