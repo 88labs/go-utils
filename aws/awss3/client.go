4f04567b5801436af43db1c5ebfad9c946c33319
tom twinkle <tomtinkle29@gmail.com>
2024-01-19 12:12:26 +0900

    change BaseEndpoint

--- aws/awss3/client.go
+++ aws/awss3/client.go
@@ -5,15 +5,14 @@ import (
 	"context"
 	"encoding/gob"
 	"fmt"
-	"net/url"
 
 	"github.com/88labs/go-utils/aws/awsconfig"
+
 	"github.com/88labs/go-utils/aws/ctxawslocal"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	smithyendpoints "github.com/aws/smithy-go/endpoints"
 )
 
 // GetClient
@@ -34,18 +33,6 @@ func GetClient(ctx context.Context, region awsconfig.Region) (*s3.Client, error)
 	return s3.NewFromConfig(awsCfg), nil
 }
 
-type staticResolver struct {
-	endpoint url.URL
-}
-
-func (r *staticResolver) ResolveEndpoint(_ context.Context, _ s3.EndpointParameters) (
-	smithyendpoints.Endpoint, error,
-) {
-	return smithyendpoints.Endpoint{
-		URI: r.endpoint,
-	}, nil
-}
-
 func getClientLocal(ctx context.Context, localProfile LocalProfile) (*s3.Client, error) {
 	awsCfg, err := awsConfig.LoadDefaultConfig(ctx,
 		awsConfig.WithCredentialsProvider(credentials.StaticCredentialsProvider{
@@ -59,23 +46,9 @@ func getClientLocal(ctx context.Context, localProfile LocalProfile) (*s3.Client,
 	if err != nil {
 		return nil, fmt.Errorf("unable to load SDK config, %w", err)
 	}
-	//u, err := url.Parse(localProfile.Endpoint)
-	//if err != nil {
-	//	return nil, fmt.Errorf("custom endpoint parse error, %w", err)
-	//}
 	return s3.NewFromConfig(awsCfg, func(o *s3.Options) {
 		o.BaseEndpoint = aws.String(localProfile.Endpoint)
-		//o.EndpointResolverV2 = &staticResolver{
-		//	endpoint: *u,
-		//}
 		o.UsePathStyle = true
-		//o.Credentials = credentials.StaticCredentialsProvider{
-		//	Value: aws.Credentials{
-		//		AccessKeyID:     localProfile.AccessKey,
-		//		SecretAccessKey: localProfile.SecretAccessKey,
-		//		SessionToken:    localProfile.SessionToken,
-		//	},
-		//}
 	}), nil
 }
 
